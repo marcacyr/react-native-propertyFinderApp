df85dab85271dc636e4b7b0efba858701ebe4203
Marc Cyr <marcacyr@gmail.com>
2015-03-31 11:09:27 -0700

    remove hello world code now that we do not need it

--- index.ios.js
+++ index.ios.js
@@ -7,22 +7,10 @@
 var React = require('react-native');
 var SearchPage = require('./SearchPage');
 var Styles = React.StyleSheet.create({
-  text: {
-    color: 'black',
-    backgroundColor: 'white',
-    fontSize: 30,
-    margin: 80
-  },
   container: {
     flex: 1
   }
 });
-
-class HelloWorld extends React.Component {
-  render() {
-    return <React.Text style={Styles.text}>Hello World!</React.Text>;
-  }
-}
 
 class PropertyFinderApp extends React.Component {
   render() {
